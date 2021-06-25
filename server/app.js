31f31634293797cbcaeb06d7b45795af6df6e41b
DeeptiEdcast <deepti4597@gmail.com>
2021-06-25 19:56:11 +0530

    Client update

--- server/app.js
+++ server/app.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express()
-const PORT = 5000
-const {MONGOURI} = require('./keys')
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = 5000;
+const { MONGOURI } = require("./keys");
 
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
-})
-mongoose.connection.on('connected', () => {
-    console.log("connected to mongo yeah");
-})
-mongoose.connection.on('error', (err) => {
+});
+mongoose.connection.on("connected", () => {
+    console.log("connected to mongo");
+});
+mongoose.connection.on("error", (err) => {
     console.log("err connecting", err);
-})
+});
 
-require('./models/user')
-require('./models/post')
+require("./models/user");
+require("./models/post");
 
-app.use(express.json())
-app.use(require('./routes/auth'))
-app.use(require('./routes/post'))
-app.use(require('./routes/user'))
+app.use(express.json());
+app.use(require("./routes/auth"));
+app.use(require("./routes/post"));
+app.use(require("./routes/user"));
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/build'))
-    const path = require('path')
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
 }
 
-app.listen(PORT, ()=>{
-    console.log('server is running on', PORT);
-})
+app.listen(PORT, () => {
+    console.log("server is running on", PORT);
+});
